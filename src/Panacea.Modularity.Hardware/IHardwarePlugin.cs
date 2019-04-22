76defb122e520819aaaaf0e3997435e7c90e64c3
Portno <j.remoundos@dotbydot.gr>
2019-04-22 13:44:13 +0300

    first commit

---
+++ src/Panacea.Modularity.Hardware/IHardwarePlugin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.Hardware
+{
+    public interface IHardwarePlugin : IPlugin
+    {
+        HardwareStatus LedState { get; set; }
+        HardwareStatus HandsetState { get; }
+        HardwareStatus BarcodeScannerState { get; set; }
+        bool SpeakersDisabled { get; set; }
+        void Stop();
+        void StartRinging();
+        void StopRinging();
+        void SetLcdBacklight(HardwareStatus status);
+        void SimulateHandsetState(HardwareStatus status);
+        void Restart();
+        event EventHandler<HardwareStatus> HandsetStateChanged;
+        event EventHandler<HardwareStatus> LcdButtonChange;
+        event EventHandler<HardwareStatus> PowerButtonChange;
+    }
+}
